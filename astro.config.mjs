86540d29dab24006d1e4507b79b5d0b3d6f5b448
alexmp.2602 <alexmp.2602@gmail.com>
2025-07-07 16:10:48 -0300

    feat: Mejora del encabezado con menú móvil y animaciones
    - Se añadió navegación móvil con menú de hamburguesa y botón de cierre.
    - Se integró Alpine.js para la gestión del estado del menú móvil.
    - Se mejoró el estilo del encabezado para una mejor respuesta y experiencia de usuario.
    - Se actualizaron los estilos globales para los estados de enfoque en enlaces y botones.
    - Se eliminó la implementación de service workers, ya que ya no es necesaria.
    - Se añadieron metaetiquetas de Open Graph y Twitter Card para un mejor SEO.
    - Se actualizó manifest.json para incluir el alcance de PWA.
    - Se añadieron nuevos iconos para el menú y las acciones de cierre.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import alpinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
   },
+  integrations: [alpinejs()],
 });
