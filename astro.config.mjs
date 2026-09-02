246e6258395e9a28c305e9c405d35306415e5067
alexmp.2602 <alexmp.2602@gmail.com>
2026-09-02 11:53:14 -0300

    refactor: mejorar la legibilidad y organización del código, actualizar imágenes y optimizar componentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
-import react from "@astrojs/react";            // por si tenés alguna isla React
+import react from "@astrojs/react"; // por si tenés alguna isla React
 import critters from "astro-critters";
 import compress from "astro-compress";
 
